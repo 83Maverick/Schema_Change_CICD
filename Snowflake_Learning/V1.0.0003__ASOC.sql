d9e34acc8bef994397b41f1dfc0d1d664a351c61
SHIVANG RAI <shivangrai1983@gmail.com>
2025-07-14 12:16:30 +0530

    Create V1.0.0003__ASOC.sql

---
+++ Snowflake_Learning/V1.0.0003__ASOC.sql
@@ -0,0 +1,8 @@
+create or replace table ASOC(
+  Name Varchar(255),
+  Role Varchar(255));
+
+insert into ASOC ('Name','Role') VALUES ('Pratik','Very Senior');
+insert into ASOC ('Name','Role') VALUES ('Mehul','Senior');
+insert into ASOC ('Name','Role') VALUES ('Shivang','Junior');
+insert into ASOC ('Name','Role') VALUES ('Tushar','Very Junior');
