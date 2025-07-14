7421c0911fb04f3de19a32fa368eb465cbbd023e
SHIVANG RAI <shivangrai1983@gmail.com>
2025-07-14 12:25:42 +0530

    Create V1.0.0004__MULTI-INSERTS.sql

---
+++ Snowflake_Learning/V1.0.0004__MULTI-INSERTS.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE TABLE member (
+   member_id INTEGER DEFAULT member_seq.nextval,
+   points_balance NUMBER,
+   started_date DATE,
+   ended_date DATE,
+   registered_date DATE,
+   firstname VARCHAR,
+   lastname VARCHAR,
+   gender VARCHAR,
+   age NUMBER,
+   email VARCHAR
+);
+
+CREATE OR REPLACE TABLE member_address (
+   member_id INTEGER,
+   street VARCHAR,
+   city VARCHAR,
+   state VARCHAR,
+   zip VARCHAR
+);
+
+CREATE OR REPLACE TABLE member_phone (
+   member_id INTEGER,
+   phone VARCHAR
+);
+
+
+
+INSERT ALL
+    INTO member(member_id, points_balance, started_date, ended_date,
+            registered_date, firstname, lastname, gender, age, email)
+    VALUES (member_id, points_balance, started_date, ended_date,
+            registered_date, firstname, lastname, gender, age, email)
+            
+    INTO member_address (member_id, street, city, state, zip)
+    VALUES (member_id, street, city, state, zip)
+    
+    INTO member_phone(member_id, phone)
+    VALUES (member_id, phone)
+    
+    SELECT member_seq.NEXTVAL AS member_id, points_balance, started_date, 
+           ended_date, registered_date, firstname, lastname, gender, age, 
+           email, street, city, state, zip, phone
+    FROM SNOWBEARAIR_DB.MODELED.MEMBERS;
